17f2c991341d2aec7ffb5126b86a4c071420c850
Krzysztof Drozdowski <k.drozdowskijg@gmail.com>
2025-04-05 13:47:39 +0000

    [LeetCode Sync] Runtime - 2 ms (31.39%), Memory - 58.9 MB (35.40%)

---
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k++] = nums[i];
+        }
+    }
+
+    return k;
+};
