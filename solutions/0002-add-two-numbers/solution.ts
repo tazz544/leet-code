3c53fea24d65fe6ae846bc06a78e26830217c527
Krzysztof Drozdowski <k.drozdowskijg@gmail.com>
2025-06-02 13:48:31 +0000

    [LeetCode Sync] Runtime - 3 ms (70.33%), Memory - 61.4 MB (93.61%)

---
+++ solutions/0002-add-two-numbers/solution.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        let val1 = (l1 !== null) ? l1.val : 0;
+        let val2 = (l2 !== null) ? l2.val : 0;
+        let sum = val1 + val2 + carry;
+
+        carry = Math.floor(sum / 10);
+
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1 !== null) {
+            l1 = l1.next;
+        }
+        if (l2 !== null) {
+            l2 = l2.next;
+        }
+    }
+    
+    return dummyHead.next;
+};
