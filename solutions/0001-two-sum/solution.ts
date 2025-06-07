3c5793ec93d9402ec5624300e505f0f4f88b1ebd
Krzysztof Drozdowski <k.drozdowskijg@gmail.com>
2025-06-07 07:59:16 +0000

    [LeetCode Sync] Runtime - 1 ms (88.41%), Memory - 57.6 MB (26.62%)

--- solutions/0001-two-sum/solution.ts
+++ solutions/0001-two-sum/solution.ts
@@ -1,15 +1,16 @@
 function twoSum(nums: number[], target: number): number[] {
-    const numsMap = new Map<number, number>();
+    const temp = new Map<number, number>();
 
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        const rest = target - num;
+        const iNum = nums[i];
+        const neededNum = target - iNum;
 
-        if (numsMap.has(rest)) {
-            return [i, numsMap.get(rest)];
+        if (temp.has(neededNum)) {
+            return [temp.get(neededNum), i];
         }
 
-        numsMap.set(num, i);
+
+        temp.set(iNum, i);
     }
 
     return [];
